afdd370f6dce7b3c6b268a5b58c1c7e0195498a5
Apollo <amporto10@gmail.com>
2023-09-22 09:53:08 -0300

    Entrada de dados

---
+++ src/main/kotlin/primeirosPassos/EntradaDeDados.kt
@@ -0,0 +1,15 @@
+package primeirosPassos
+
+fun main(){
+
+    println("Qual é o seu nome?")
+
+    var nome: String = readLine()!!.toString()
+
+    println("Olá, $nome. Muito prazer.")
+    println("Qual é sua idade?")
+
+    var idade: Int = readLine()!!.toInt()
+
+    println("Sua idade é $idade")
+}
