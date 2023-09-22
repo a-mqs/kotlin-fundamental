d7b28a3b953339128e5de86ead34486a21184f6d
Apollo <amporto10@gmail.com>
2023-09-21 22:32:11 -0300

    Calculo de idade, formatado em melhores praticas

--- src/main/kotlin/primeirosPassos/CalcularAnosDeVida.kt
+++ src/main/kotlin/primeirosPassos/CalcularAnosDeVida.kt
@@ -6,14 +6,25 @@ fun main(){
     var anoDeNascimento = 2000
     var anoAtual = 2023
 
-    // Processamento
+    calcularAnosDeVida(anoDeNascimento, anoAtual);
+
+    imprimirResultados(anoDeNascimento, anoAtual)
+
+}
+
+fun calcularAnosDeVida(anoDeNascimento: Int, anoAtual: Int): Int {
+
     var anosDeVida = anoAtual - anoDeNascimento
 
-    // Saída / Resultados
+    return anosDeVida
+}
+
+fun imprimirResultados(anoDeNascimento: Int, anoAtual: Int){
     print("Como calcular quantos anos de vida uma pessoa tem?\n\n")
     print("Ano de nascimento: $anoDeNascimento\n")
     print("Ano de atual: $anoAtual\n")
-    print("Resultado: $anosDeVida\n")
-    print("Você tem $anosDeVida anos de vida\n")
+    print("Resultado: ${calcularAnosDeVida(anoDeNascimento, anoAtual)}\n")
+    print("Você tem ${calcularAnosDeVida(anoDeNascimento, anoAtual)} anos de vida\n")
+
 
 }
