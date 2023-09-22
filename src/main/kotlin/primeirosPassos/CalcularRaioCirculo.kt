b15b9a4d3d63a7ab01b0b0a179190040f5b16473
Apollo <amporto10@gmail.com>
2023-09-22 09:45:48 -0300

    Calculo de raio de circulo

---
+++ src/main/kotlin/primeirosPassos/CalcularRaioCirculo.kt
@@ -0,0 +1,12 @@
+const val PI = 3.141516
+fun main(){
+
+    var circulo = 15
+    // val pi = 3.141516
+
+    var valorDoRaio = circulo / (2*PI)
+
+    print("Círculo mede: $circulo\n")
+    print("Valor do raio: $valorDoRaio\n")
+
+}
