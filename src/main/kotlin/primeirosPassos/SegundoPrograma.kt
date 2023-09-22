b1c43c8a0d051ae72c9ff4e7416d8e7011ae7a58
Apollo <amporto10@gmail.com>
2023-09-21 22:07:52 -0300

    Segundo programa

---
+++ src/main/kotlin/primeirosPassos/SegundoPrograma.kt
@@ -0,0 +1,17 @@
+package primeirosPassos
+
+fun main(){
+
+    var variavelA = "Cidade"
+    var variavelB = "Natal"
+
+    imprimir(variavelA, variavelB)
+    imprimir(variavelB, variavelA)
+
+}
+
+fun imprimir(varA: String, varB: String){
+
+    print("$varA\n$varB\n")
+
+}
