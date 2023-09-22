e639be8253cddbfb1d0b7f3f6f56668d59f74518
Apollo <amporto10@gmail.com>
2023-09-21 22:13:04 -0300

    Calculo de idade

---
+++ src/main/kotlin/primeirosPassos/CalcularAnosDeVida.kt
@@ -0,0 +1,19 @@
+package primeirosPassos
+
+fun main(){
+
+    // Entrada de dados
+    var anoDeNascimento = 2000
+    var anoAtual = 2023
+
+    // Processamento
+    var anosDeVida = anoAtual - anoDeNascimento
+
+    // Saída / Resultados
+    print("Como calcular quantos anos de vida uma pessoa tem?\n\n")
+    print("Ano de nascimento: $anoDeNascimento\n")
+    print("Ano de atual: $anoAtual\n")
+    print("Resultado: $anosDeVida\n")
+    print("Você tem $anosDeVida anos de vida\n")
+
+}
