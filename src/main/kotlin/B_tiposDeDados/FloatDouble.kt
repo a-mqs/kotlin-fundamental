508212722492f223a679d05bd3a3e1446dbb8592
Apollo <amporto10@gmail.com>
2023-09-25 10:16:07 -0300

    Float e Double

---
+++ src/main/kotlin/B_tiposDeDados/FloatDouble.kt
@@ -0,0 +1,12 @@
+package B_tiposDeDados
+fun main(){
+    var preco: Float = 3.8765433f
+    var precoGasolina: Double = 5.54321
+
+    println("float valor min: ${Float.MIN_VALUE}")
+    println("float valor max: ${Float.MAX_VALUE}\n")
+
+    println("double valor min: ${Double.MIN_VALUE}")
+    println("double valor max: ${Double.MAX_VALUE}\n")
+
+}
