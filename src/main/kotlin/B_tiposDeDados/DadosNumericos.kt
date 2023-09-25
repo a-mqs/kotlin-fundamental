02f407b8c5c8c7252e22159bae7ff7aba67cab7a
Apollo <amporto10@gmail.com>
2023-09-25 09:36:16 -0300

    Dados numéricos

---
+++ src/main/kotlin/B_tiposDeDados/DadosNumericos.kt
@@ -0,0 +1,24 @@
+fun main(){
+
+    val byte: Byte = 127;
+    val int: Int = 1000;
+    val short: Short = -32768;
+    val long: Long = 3000;
+
+    println("Byte: $byte")
+    println("Byte valor Min: ${Byte.MIN_VALUE}")
+    println("Byte valor Max: ${Byte.MAX_VALUE}\n")
+
+    println("Int: $int")
+    println("Int valor Min: ${Int.MIN_VALUE}")
+    println("Int valor Max: ${Int.MAX_VALUE}\n")
+
+    println("Short: $short")
+    println("Short valor Min: ${Short.MIN_VALUE}")
+    println("Short valor Max: ${Short.MAX_VALUE}\n")
+
+    println("Long: $long")
+    println("Long valor Min: ${Long.MIN_VALUE}")
+    println("Long valor Max: ${Long.MAX_VALUE}\n")
+
+}
