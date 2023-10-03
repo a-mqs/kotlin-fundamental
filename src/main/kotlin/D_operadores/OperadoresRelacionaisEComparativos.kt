b61ab9b17dc6887714ac5e0f8798d80d4dd98d3e
Apollo <amporto10@gmail.com>
2023-10-03 10:20:29 -0300

    Operadores relacionais e comparativos

---
+++ src/main/kotlin/D_operadores/OperadoresRelacionaisEComparativos.kt
@@ -0,0 +1,27 @@
+package D_operadores
+fun main(){
+
+    var valorA = 10;
+    var valorB = 10;
+
+    println("A é igual a B?: ${valorA == valorB}")
+    println("A é igual a B a nível de ser um objeto?: ${valorA === valorB}\n")
+
+    var valorC = 10;
+    var valorD = 20;
+
+    println("C é igual a D?: ${valorC == valorD}")
+    println("A é igual a B a nível de ser um objeto?: ${valorC === valorD}")
+
+    println("C é igual a D?: ${valorC == valorD}")
+    println("A é igual a B a nível de ser um objeto?: ${valorC === valorD}")
+
+    var valorE = "Campo Grande";
+    var valorF = "Brasília";
+
+    var valorG = "Campo Grande";
+    var valorH = "Brasília";
+
+
+
+}
